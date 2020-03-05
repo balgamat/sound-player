7a20caffb6d1fc6e248aa67f7183f55195fcaa67
Matěj Balga <matej.balga@icloud.com>
2020-03-05 19:58:04 +0100

    Published.

--- example.js
+++ example.js
@@ -4,10 +4,10 @@ const filenames = process.argv.slice(2);
 let a = true;
 
 const channels = filenames.map(f => {
-  const channel = new SoundPlayer({onClose: () => {console.log('Ended')}});
+  const channel = new SoundPlayer({device: 'hw1,0',onFinished: () => {console.log('Ended')}});
   channel.play({ filename: f });
   return channel;
 });
 
-setTimeout(() => channels[0].play({ filename: filenames[0], volume: 10 }), 10000);
+setTimeout(() => channels[0].play({ filename: filenames[0] }), 2000);
 
--- index.ts
+++ index.ts
@@ -5,27 +5,33 @@ import { Decoder, STEREO } from "lame";
 const Speaker = require("speaker");
 const volumeControl = require("pcm-volume");
 
+process.on("unhandledRejection", () => {});
+process.on("warning", () => {});
+
 class SoundPlayer {
-  public readonly device?: ISoundDevice | null;
+  public readonly device?: string | null;
   private _volume: any;
   private _volumeValue: number = 100;
   private _speaker: any;
   private _decoder: Decoder;
-  private readonly onClose: () => void;
-  private readonly onError: () => void;
+  private _fs: fs.ReadStream | null;
+  private readonly onFinished: () => void;
+  private readonly onError: (e: any) => void;
 
   constructor({
     device = null,
-    onClose = () => {},
-    onError = () => {}
+    onFinished = () => {},
+    onError = (_) => {}
   }: ISoundPlayer = {}) {
     this.device = device;
-    this.onClose = onClose;
+    this.onFinished = onFinished;
     this.onError = onError;
     this._speaker = new Speaker({
       // @ts-ignore
-      device: device?.address
+      device
     });
+    console.log(`Using device: ${device}`);
+    this._fs = null;
     this._decoder = new Decoder({
       channels: 2,
       bitDepth: 16,
@@ -59,39 +65,55 @@ class SoundPlayer {
     }
   }
 
-  play({ filename, volume = 100 }: IPlay) {
-    this._speaker = new Speaker({
-      // @ts-ignore
-      device: this.device?.address || null
-    });
-    this._decoder = new Decoder({
-      channels: 2,
-      bitDepth: 16,
-      sampleRate: 44100,
-      bitRate: 128,
-      outSampleRate: 22050,
-      mode: STEREO
-    });
-    // @ts-ignore
-    this._volume = new volumeControl();
+  play({ filename, volume = 100, loop = false }: IPlay) {
+    try {
+      this._speaker.close();
+    } catch (e) { this.onError(e) }
 
     try {
-      const fileStream = fs.createReadStream(filename);
-      this._decoder.pipe(this._volume);
-      this._volume.pipe(this._speaker);
-      fileStream.pipe(this._decoder);
-      this._speaker.on("flush", () => {
-        console.log(`${filename} ended.`);
-        this._speaker.close();
+      this._speaker = new Speaker({
+        // @ts-ignore
+        device: this.device?.address || null
       });
-      this.volume = volume;
-    } catch {}
+      this._decoder = new Decoder({
+        channels: 2,
+        bitDepth: 16,
+        sampleRate: 44100,
+        bitRate: 128,
+        outSampleRate: 22050,
+        mode: STEREO
+      });
+      // @ts-ignore
+      this._volume = new volumeControl();
+
+      const fileStream = fs.createReadStream(filename);
+      fileStream.pipe(this._decoder);
+      this._decoder.on('format', (f: any) => {
+        this._speaker = new Speaker({
+          // @ts-ignore
+          device: this.device?.address || null,
+          sampleRate: f.sampleRate,
+          channels: f.channels,
+          bitDepth: f.bitDepth
+        });
+
+        this._speaker.on("flush", () => {
+          this.onFinished();
+          this._speaker.close();
+          !!loop && this.play({filename, volume, loop});
+        });
+        this.volume = volume;
+
+        this._decoder.pipe(this._volume);
+        this._volume.pipe(this._speaker);
+      });
+    } catch (e) { this.onError(e) }
   }
 
   stop() {
     try {
       this._speaker.close();
-    } catch {}
+    } catch (e) { this.onError(e) }
   }
 
   public get volume() {
@@ -109,15 +131,16 @@ export { SoundPlayer };
 export interface IPlay {
   filename: string;
   volume?: number;
+  loop?: boolean;
+}
+
+export interface ISoundPlayer {
+  device?: string | null;
+  onFinished?: () => void;
+  onError?: (data?: any) => void;
 }
 
 export interface ISoundDevice {
   address: string;
   name: string;
-}
-
-export interface ISoundPlayer {
-  device?: ISoundDevice | null;
-  onClose?: () => void;
-  onError?: (data?: any) => void;
 }
